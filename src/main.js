771a29c0d2ca6b0cc919baa5126b6e2a1c6d5f7e
shangxianw <997049907@qq.com>
2022-10-01 02:39:07 +0800

    添加vant

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
-createApp(App).mount('#app')
+createApp(App).use(Vant).mount('#app')
