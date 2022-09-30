afa1040bbe3a4a40762fdc95e339baaef117cf53
shangxianw <997049907@qq.com>
2022-10-01 04:26:21 +0800

    登录鉴权监听

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import axios from "axios";
 const app = createApp(App)
 axios.defaults.withCredentials = true;
 app.config.globalProperties.$api = axios;
+axios.interceptors.response.use(config => {
+  // cookie相关
+  if ([1001, 1002, 1003, 1004].includes(Number(config?.data?.code))) {
+    location.href = "#/login";
+  }
+  return config;
+})
 
 app.use(Vant)
 app.use(router).mount('#app')
