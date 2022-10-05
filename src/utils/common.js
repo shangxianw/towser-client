73f47763b859b72fe48ef9eeabb97c4b7faf9a04
shangxianw <997049907@qq.com>
2022-10-05 13:48:30 +0800

    整理cookie方法到公告文件

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,48 +1,36 @@
-const obj = {}
-obj.milSecond = 1;
-obj.second = obj.milSecond * 1000;
-obj.minute = obj.second * 60;
-obj.hour = obj.minute * 60;
-obj.day = obj.hour * 24;
+function setCookie(key, value) {
+  const cookie = document.cookie;
+  const objs = cookie.split(";");
+  const obj = {}
+  objs.forEach(item => {
+    const { key, value } = item.split("=");
+    if (key == null || value == null) return;
+    obj.key = key;
+    obj.value = value;
+  })
+  obj[key] = value;
 
-function timeStamp2LeftTime(t) {
-  let tmpTime = t;
-  let format = ``;
-  const day = Math.floor(tmpTime / obj.day);
-  if (day > 0) {
-    format += `${day}天`;
-    tmpTime -= obj.day * day;
-  } else if (format) {
-    format += `0天`;
+  // 还原
+  let strs = [];
+  for (let i in obj) {
+    strs.push(`${i}=${obj[i]}`);
   }
+  document.cookie = strs.join(";");
+}
 
-  const hour = Math.floor(tmpTime / obj.hour);
-  if (hour > 0) {
-    format += `${hour}小时`;
-    tmpTime -= obj.hour * hour;
-  } else if (format) {
-    format += `0小时`;
+function getCookie(key) {
+  const cookie = document.cookie;
+  const objs = cookie.split(";");
+  for (let i = 0; i < objs.length; i++) {
+    const [k, v] = objs[i].split("=");
+    if (key === k.trim()) {
+      return v.trim();
+    }
   }
-
-  const minute = Math.floor(tmpTime / obj.minute);
-  if (minute > 0) {
-    format += `${minute}分`;
-    tmpTime -= obj.minute * minute;
-  } else if (format) {
-    format += `0分`;
-  }
-
-  const second = Math.floor(tmpTime / obj.second);
-  if (second > 0) {
-    format += `${second}秒`;
-    tmpTime -= obj.second * second;
-  } else if (format) {
-    format += `0秒`;
-  }
-  tmpTime;
-  return format;
+  return "";
 }
 
 export default {
-  timeStamp2LeftTime
+  setCookie,
+  getCookie
 }
